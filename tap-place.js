dde1715d6ef778ebecaa1dff24ce931ff2b88c2d
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-09 13:53:36 +0530

    snapshot

---
+++ tap-place.js
@@ -0,0 +1,22 @@
+const sceneEl = document.querySelector('a-scene');
+const faq = document.getElementById('faq')
+faq.addEventListener('click', (event) => {
+
+console.log("snap target clicked");
+// Create new entity for the new object
+const newElement = document.createElement('a-entity')
+// The raycaster gives a location of the touch in the scene
+ const touchPoint = event.detail.intersection.point
+ console.log(touchPoint);
+ newElement.setAttribute('position', touchPoint)
+ newElement.setAttribute('visible', 'false')
+ newElement.setAttribute('scale', '0.01 0.01 0.01')
+ newElement.setAttribute('gltf-model', '#camera-icon')
+ snaptarget.appendChild(newElement)
+ newElement.setAttribute('position', '0 -0.3 0')
+//  newElement.setAttribute('offset', '-455 -401 2975')
+
+ newElement.addEventListener('model-loaded', () => {
+ newElement.setAttribute('visible', 'true')
+})
+});
